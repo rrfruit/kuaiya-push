33ff3ba9753e728a14c154878b2152f6ccf6d782
renkun <renkun@mail.net>
2025-12-02 18:12:19 +0800

    11

---
+++ apps/web/src/components/layouts/authenticated-layout.tsx
@@ -0,0 +1,27 @@
+import { AppSidebar } from "./app-sidebar";
+import { SiteHeader } from "./site-header";
+import { SidebarInset, SidebarProvider } from "@repo/ui/components/ui/sidebar";
+import { Outlet } from "react-router-dom";
+
+export default function AuthenticatedLayout() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 64)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <Outlet />
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
