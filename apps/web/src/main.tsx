6a77d167df6ef774e6791290aa6c410b0671a6f3
renkun <renkun@mail.net>
2025-12-02 11:18:55 +0800

    11

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,9 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./router/router";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
