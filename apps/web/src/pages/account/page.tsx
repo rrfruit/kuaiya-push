38faeaeffd7cc261900b45a9f109016ee96b1afa
renkun <renkun@mail.net>
2025-12-04 15:29:17 +0800

    11

--- apps/web/src/pages/account/page.tsx
+++ apps/web/src/pages/account/page.tsx
@@ -45,7 +45,7 @@ export default function AccountPage() {
   }
 
   return (
-    <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+    <div className="hidden h-full flex-1 flex-col space-y-4 p-8 md:flex">
       <div className="flex items-center justify-between space-y-2">
         <div>
           <h2 className="text-2xl font-bold tracking-tight">账号管理</h2>
