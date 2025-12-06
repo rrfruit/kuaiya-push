6e4aea79f98ead8a11c0049125d7c936da851fb6
renkun <renkun@mail.net>
2025-12-06 11:26:43 +0800

    111

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,19 +1,14 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { Toaster } from 'sonner';
+import { Toaster } from "sonner";
 
 import { router } from "./router/router";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Toaster position="top-center" richColors />
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <Toaster position="top-center" richColors />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
